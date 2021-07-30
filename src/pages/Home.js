f04bff58b1cf6557d928dcc1b50b5746ff1a4da7
TonyBarbou <tonybarbou@live.fr>
2021-07-30 13:50:33 +0200

    Removing header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,9 +11,6 @@ function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
 
-      {/*  Site header */}
-      <Header />
-
       {/*  Page content */}
       <main className="flex-grow">
 
