f761a5e4420837ae282feaf475253a49daea21c1
TonyBarbou <tonybarbou@live.fr>
2021-07-30 17:59:59 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -7,13 +7,6 @@ import ReactGA from 'react-ga';
 
 ReactGA.initialize('UA-203672436-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
-const history = createBrowserHistory();
-
-// Initialize google analytics page view tracking
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
 
 ReactDOM.render(
   <React.StrictMode>
